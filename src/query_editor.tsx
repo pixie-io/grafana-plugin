ae328d43fc6f27a691da3f0716d4ff6ed682409d
Taras Priadka <tpriadka@pixielabs.ai>
2022-06-03 13:57:21 -0700

    Remove unnecessary conversion to map with scripts. (#43)
    Signed-off-by: Taras Priadka <tpriadka@pixielabs.ai>

--- src/query_editor.tsx
+++ src/query_editor.tsx
@@ -22,7 +22,7 @@ import React, { ChangeEvent, PureComponent } from 'react';
 import { TextArea, Select } from '@grafana/ui';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from './datasource';
-import { scriptOptions, scripts } from 'pxl_scripts';
+import { scriptOptions } from 'pxl_scripts';
 import { defaultQuery, PixieDataSourceOptions, PixieDataQuery } from './types';
 
 type Props = QueryEditorProps<DataSource, PixieDataQuery, PixieDataSourceOptions>;
@@ -34,16 +34,11 @@ export class QueryEditor extends PureComponent<Props> {
     onRunQuery();
   }
 
-  onScriptSelect(option: SelectableValue<number>) {
-    if (option.label != null) {
-      // Load in predefined scripts to the script text box
-
-      const script = scripts.get(option.label);
-      if (script !== undefined) {
-        const { onChange, query, onRunQuery } = this.props;
-        onChange({ ...query, pxlScript: script.script });
-        onRunQuery();
-      }
+  onScriptSelect(option: SelectableValue<string>) {
+    if (option.value !== undefined) {
+      const { onChange, query, onRunQuery } = this.props;
+      onChange({ ...query, pxlScript: option.value });
+      onRunQuery();
     }
   }
 
