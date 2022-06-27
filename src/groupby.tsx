e0dbb2084949d055b8d44c772a1ed63057493cc0
Kartik Pattaswamy <kpattaswamy@pixielabs.ai>
2022-06-27 10:28:01 -0700

    Added license to groupby.tsx
    Signed-off-by: Kartik Pattaswamy <kpattaswamy@pixielabs.ai>

--- src/groupby.tsx
+++ src/groupby.tsx
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { SelectableValue } from '@grafana/data';
 
 export const aggFunctionOptions: Array<{ label: string; value: number }> = [
@@ -53,10 +71,10 @@ export function getGroupByScript(
 
 export function getAggValues(name: string): { label: string; value: number } | undefined {
   if (name === '') {
-      // Select value wasn't chosen so must display placeholder
+    // Select value wasn't chosen so must display placeholder
     return undefined;
   } else {
-      // Placeholder should not be displayed
+    // Placeholder should not be displayed
     return { label: name, value: 0 };
   }
 }
